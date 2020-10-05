7ed6e33a6e6def36bae9d530c84a70b4f66266d1
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-10-05 20:54:23 +0400

    Added home and login page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,39 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+    {{ content }}
+    {{ $auth.user }}
+    {{ $auth.loggedIn }}
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+    <div style="background-color: black;" />
+    <div style="margin-top: 100em">
+      ex
     </div>
   </section>
 </template>
 
-<script>
-import Card from '~/components/Card'
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
-export default {
-  name: 'HomePage',
+import Card from '~/components/Card.vue'
 
+export default defineComponent({
+  name: 'Index',
   components: {
     Card
+  },
+
+  setup () {
+    const content = ref('Lorem Ipsum')
+
+    return {
+      content
+    }
+  },
+
+  head () {
+    return {
+      title: 'Home ✨'
+    }
   }
-}
+})
 </script>
---
+++ pages/login.vue
@@ -0,0 +1,120 @@
+<template>
+  <section id="login" class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="card form has-text-left">
+          <div class="card-header">
+            <p class="card-header-title">
+              Login
+            </p>
+            <a
+              class="card-header-icon"
+              @click="$router.go(-1)"
+            ><b-icon icon="close" />
+            </a>
+          </div>
+
+          <div class="card-content">
+            <b-field label="Email" label-position="inside">
+              <b-input
+                v-model="state.email"
+                type="email"
+              />
+            </b-field>
+
+            <b-field
+              label="Password"
+              label-position="inside"
+              required
+            >
+              <b-input
+                v-model="state.password"
+                type="password"
+              />
+            </b-field>
+
+            <b-notification v-show="errorMsg" type="is-danger is-light">
+              {{ errorMsg }}
+            </b-notification>
+
+            <b-button
+              type="is-info login-btn"
+              expanded
+              @click="authenticate"
+            >
+              Login
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'login',
+  layout: 'empty',
+  auth: 'guest',
+
+  setup () {
+    const state = reactive({
+      email: '',
+      password: ''
+    })
+
+    const errorMsg = ref('')
+
+    function authenticate () {
+      // @ts-ignore
+      this.$auth.loginWith('local', {
+        data: {
+          email: state.email,
+          password: state.password
+        }
+      }).then((res: any) => {
+        const resData = res.data
+
+        // @ts-ignore
+        this.$auth.setUser(resData.user)
+        // @ts-ignore
+        this.$auth.setUserToken(resData.jwt.token)
+      }).catch((err: any) => {
+        errorMsg.value = err.response.data.message
+      })
+    }
+
+    return {
+      state,
+      errorMsg,
+      authenticate
+    }
+  },
+
+  head () {
+    return {
+      title: 'Login'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+#login {
+  .form {
+    max-width: 32em;
+    margin: 0 auto;
+    border-bottom-right-radius: 2em;
+  }
+
+  .card-header {
+    box-shadow: 0px;
+  }
+
+  .login-btn {
+    border-bottom-right-radius: 1em;
+  }
+}
+</style>
