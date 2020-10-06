e89eff0929c4fd7f8b8aa9fe6ebb51f0dbd5ca76
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-10-06 05:07:41 +0400

    Added paddign on navbar and remove classes.

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,73 @@
 <template>
-  <section class="section">
-    {{ content }}
-    {{ $auth.user }}
-    {{ $auth.loggedIn }}
+  <div id="index">
+    <section class="hero">
+      <div v-if="posts.length > 0" class="hero-body">
+        <div class="container">
+          <div class="subtitle oswald header">
+            Latest Posts
 
-    <div style="background-color: black;" />
-    <div style="margin-top: 100em">
-      ex
-    </div>
-  </section>
+            <a class="header-right">
+              View All
+              <b-icon icon="view-list" />
+            </a>
+          </div>
+          <hr>
+
+          <div class="columns">
+            <div
+              v-for="post in posts.slice(0, 3)"
+              :key="post.id"
+
+              class="column"
+            >
+              <Post
+                :id="post.id"
+                :title="post.title"
+                :image="post.imageUrl"
+                :date="post.createdAt"
+                :name="user.fullName"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent, onMounted, ref, useContext } from '@nuxtjs/composition-api'
 
-import Card from '~/components/Card.vue'
+import Post from '~/components/Index/Post.vue'
 
 export default defineComponent({
-  name: 'Index',
+  name: 'index',
   components: {
-    Card
+    Post
   },
 
   setup () {
-    const content = ref('Lorem Ipsum')
+    const { $axios } = useContext()
+
+    const user = ref({
+      bio: String,
+      email: String,
+      fullName: String,
+      joinedAt: String,
+      posts: []
+    })
+
+    const posts = ref([])
+
+    onMounted(async () => {
+      const resp = await $axios.$get('https://zeth-juno.herokuapp.com/user/zeth.leonardo@protonmail.com?includePosts=true')
+      user.value = resp.user
+      posts.value = resp.user.posts.reverse()
+    })
 
     return {
-      content
+      user,
+      posts
     }
   },
 
@@ -37,3 +78,17 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+#index {
+  .br {
+    border-radius: 1em;
+  }
+
+  .header {
+    &-right {
+      float: right;
+    }
+  }
+}
+</style>
